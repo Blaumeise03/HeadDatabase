b8813e419cfeafb96c4b6a0b99e6cb1e3f0395e1
Blaumeise03 <bluegame61@gmail.com>
2019-11-02 11:44:30 +0100

    Update
    Signed-off-by: Blaumeise03 <bluegame61@gmail.com>

--- src/main/java/de/blaumeise03/headDatabase/Head.java
+++ src/main/java/de/blaumeise03/headDatabase/Head.java
@@ -1,18 +1,5 @@
 /*
- *     Copyright (C) 2019  Blaumeise03 - bluegame61@gmail.com
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as published
- *     by the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
- *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (c) 2019 Blaumeise03
  */
 
 package de.blaumeise03.headDatabase;
@@ -21,7 +8,10 @@ import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.properties.Property;
 import de.blaumeise03.headDatabase.menu.MenuButton;
 import de.blaumeise03.headDatabase.menu.MenuSession;
-import org.bukkit.*;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.SoundCategory;
 import org.bukkit.craftbukkit.libs.org.apache.commons.codec.binary.Base64;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
--- src/main/java/de/blaumeise03/headDatabase/HeadDatabase.java
+++ src/main/java/de/blaumeise03/headDatabase/HeadDatabase.java
@@ -1,18 +1,5 @@
 /*
- *     Copyright (C) 2019  Blaumeise03 - bluegame61@gmail.com
- *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Affero General Public License as published
- *     by the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU Affero General Public License for more details.
- *
- *     You should have received a copy of the GNU Affero General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Copyright (c) 2019 Blaumeise03
  */
 
 package de.blaumeise03.headDatabase;
@@ -22,9 +9,7 @@ import de.blaumeise03.headDatabase.menu.MenuSession;
 import de.blaumeise03.spigotUtils.AdvancedPlugin;
 import de.blaumeise03.spigotUtils.Command;
 import de.blaumeise03.spigotUtils.Configuration;
-import de.blaumeise03.spigotUtils.exceptions.CommandNotFoundException;
 import de.blaumeise03.spigotUtils.exceptions.ConfigurationNotFoundException;
-import de.blaumeise03.spigotUtils.exceptions.PluginNotDefinedException;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
@@ -73,65 +58,62 @@ public class HeadDatabase extends AdvancedPlugin {
         getLogger().info("Registering events...");
         registerEvent(new MenuListener());
         getLogger().info("Registering commands...");
-        try {
-            new Command("head","Sehe /head help", new Permission("head.head"), true) {
-                @Override
-                public void onCommand(String[] args, CommandSender sender) {
-                    if(args.length == 2){
-                        if(args[0].equalsIgnoreCase("LINK")) {
-                            Player p = (Player) sender;
-                            p.getInventory().addItem(Head.getSkull(args[1]));
-                            p.sendMessage("§aHier ist dein Kopf.");
-                            return;
-                        }if(args[0].equalsIgnoreCase("PLAYER")){
-                            OfflinePlayer o = Bukkit.getOfflinePlayer(args[1]);
-                            Player p = (Player) sender;
-                            ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
-                            SkullMeta iMeta = (SkullMeta) skull.getItemMeta();
-                            assert iMeta != null;
-                            iMeta.setOwningPlayer(o);
-                            skull.setItemMeta(iMeta);
-                            p.getInventory().addItem(skull);
-                            sender.sendMessage("§aHier ist dein Kopf.");
-                            return;
-                        }
-                    }else if(args.length == 1){
-                        if(args[0].equalsIgnoreCase("help")){
-                            sender.sendMessage("§aHeadDatabase - Hilfe\n" +
-                                    "§2/head §a - Öffnet das Menu (WIP).\n" +
-                                    "§2/head LINK <Link> §a-Gibt dir den Kopf von der Minecraft-URL (eine längere Zeichenkette oder die ganze Adresse).\n" +
-                                    "§2/head PLAYER <Spielername> §a-Gibt dir den Kopf des Spielers.\n" +
-                                    "§2/head reload §a-Lädt die Config neu. §cDies §4LÖSCHT§c den Cache!!\n" +
-                                    "§2/head preventSave §a-Verhindert das Speichern während des neuladens.");
-                            return;
-                        }
-                        if(args[0].equalsIgnoreCase("preventSave")){
-                            preventSave = !preventSave;
-                            sender.sendMessage("§aDie Config wird beim Neustart/Stop" + (preventSave ? "§c nicht§a " : " §2") + "gespeichert.");
-                            return;
-                        }
-                        if(args[0].equalsIgnoreCase("reload")){
-                            getLogger().info("Reloading configuration...");
-                            config.reload();
-                            getLogger().info("Reloading heads...");
-                            heads = HeadCollection.loadAll(config);
-                            getLogger().warning("Reloaded " + HeadCollection.getHeadCount() + " heads!");
-                            getLogger().info("Regenerating menu...");
-                            HeadCollection.generateMenu();
-                            sender.sendMessage("§aKöpfe wurden neugeladen!");
-                            return;
-                        }
-                    }
-                    if(args.length == 0){
-                        sender.sendMessage("§aÖffne Menü...");
-                        new MenuSession(HeadCollection.headDatabaseMenu, (Player) sender, 45, "Datenbank");
+        new Command(getHandler(), "head", "Sehe /head help", new Permission("head.head"), true) {
+            @Override
+            public void onCommand(String[] args, CommandSender sender) {
+                if (args.length == 2) {
+                    if (args[0].equalsIgnoreCase("LINK")) {
+                        Player p = (Player) sender;
+                        p.getInventory().addItem(Head.getSkull(args[1]));
+                        p.sendMessage("§aHier ist dein Kopf.");
                         return;
                     }
-                    sender.sendMessage("§cZu wenig argumente! Gebe §2/head help§c ein für weitere Hilfe.");
+                    if (args[0].equalsIgnoreCase("PLAYER")) {
+                        OfflinePlayer o = Bukkit.getOfflinePlayer(args[1]);
+                        Player p = (Player) sender;
+                        ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+                        SkullMeta iMeta = (SkullMeta) skull.getItemMeta();
+                        assert iMeta != null;
+                        iMeta.setOwningPlayer(o);
+                        skull.setItemMeta(iMeta);
+                        p.getInventory().addItem(skull);
+                        sender.sendMessage("§aHier ist dein Kopf.");
+                        return;
+                    }
+                } else if (args.length == 1) {
+                    if (args[0].equalsIgnoreCase("help")) {
+                        sender.sendMessage("§aHeadDatabase - Hilfe\n" +
+                                "§2/head §a - Öffnet das Menu (WIP).\n" +
+                                "§2/head LINK <Link> §a-Gibt dir den Kopf von der Minecraft-URL (eine längere Zeichenkette oder die ganze Adresse).\n" +
+                                "§2/head PLAYER <Spielername> §a-Gibt dir den Kopf des Spielers.\n" +
+                                "§2/head reload §a-Lädt die Config neu. §cDies §4LÖSCHT§c den Cache!!\n" +
+                                "§2/head preventSave §a-Verhindert das Speichern während des neuladens.");
+                        return;
+                    }
+                    if (args[0].equalsIgnoreCase("preventSave")) {
+                        preventSave = !preventSave;
+                        sender.sendMessage("§aDie Config wird beim Neustart/Stop" + (preventSave ? "§c nicht§a " : " §2") + "gespeichert.");
+                        return;
+                    }
+                    if (args[0].equalsIgnoreCase("reload")) {
+                        getLogger().info("Reloading configuration...");
+                        config.reload();
+                        getLogger().info("Reloading heads...");
+                        heads = HeadCollection.loadAll(config);
+                        getLogger().warning("Reloaded " + HeadCollection.getHeadCount() + " heads!");
+                        getLogger().info("Regenerating menu...");
+                        HeadCollection.generateMenu();
+                        sender.sendMessage("§aKöpfe wurden neugeladen!");
+                        return;
+                    }
                 }
-            };
-        } catch (PluginNotDefinedException | CommandNotFoundException e) {
-            e.printStackTrace();
-        }
+                if (args.length == 0) {
+                    sender.sendMessage("§aÖffne Menü...");
+                    new MenuSession(HeadCollection.headDatabaseMenu, (Player) sender, 45, "Datenbank");
+                    return;
+                }
+                sender.sendMessage("§cZu wenig argumente! Gebe §2/head help§c ein für weitere Hilfe.");
+            }
+        };
     }
 }
